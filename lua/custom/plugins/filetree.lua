bdea61932f6e4fa0d7a42e4f783ed3532654ed7a
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2024-02-19 23:13:32 +0000

    some twiks

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,28 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function ()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+          hide_by_name = {
+            ".idea",
+            ".git",
+          }
+        }
+      }
+    }
+  end,
+}
