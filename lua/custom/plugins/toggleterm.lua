91a358686a039af6bb365c630484040771ab0bb2
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2024-04-25 00:20:38 +0100

    more settings

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,5 +3,8 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup {}
+
+    vim.keymap.set('i', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+    vim.keymap.set('n', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
   end,
 }
